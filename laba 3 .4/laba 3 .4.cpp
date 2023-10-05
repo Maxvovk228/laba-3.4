880d6868d11f77a8a4c7dfebb71a6835388319a2
Maxvovk228 <145869483+Maxvovk228@users.noreply.github.com>
2023-10-05 10:56:39 +0300

    first commit

---
+++ laba 3 .4/laba 3 .4.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	double x; // вхідний аргумент
+	double y; // вхідний параметр
+	double R; // вхідний параметр
+	cout << "x = "; cin >> x;
+	cout << "y = "; cin >> y;
+	cout << "R = "; cin >> R;
+
+	if (( x <= 0 && y <= x + R && y>=0) ||
+		( x >= 0 && y >= sqrt ( R * R - x * x ) && y >= 0) ||
+		( x <= 0 && y >= sqrt (R * R - x * x) && y <= 0)
+		)
+		cout << "yes" << endl;
+	else
+		cout << "no" << endl;
+	cin.get();
+	return 0;
+}
